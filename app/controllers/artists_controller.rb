5c09534920f704142d416681ecdd28211fec5920
Steph Mahnke <mahnkes1@msu.edu>
2021-06-01 10:58:11 -0600

    added all controller actions

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,13 +1,60 @@
 class ArtistsController < ApplicationController
+  # Since it's a child controller, change Model_name -> @parent.model_names
+  # Also need before action to define @parent
+  # for index, no .all
+  before_action :set_billboard
+  before_action :set_artist, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @artists = @billboard.artists
+    render component: 'Artists', props: {artists: @artists, billboard: @billboard}
   end
 
   def show
+    render component: 'Artist', props: {artist: @artist, billboard: @billboard}
   end
 
   def new
+    @artist = @billboard.artist.new
+    render component: 'ArtistNew', props: {artist: @artist, billboard: @billboard}
+  end
+
+  def create
+    @artist = @billboard.artists.new(artist_params)
+    if @artist.save
+      redirect_to root_path
+    else
+      render component: 'ArtistNew', props: {artist: @artist, billboard: @billboard}
+    end
   end
 
   def edit
+    render component: 'ArtistEdit', props: {artist: @artist, billboard: @billboard}
   end
+
+  def update
+    if @artist.update(artist_params)
+      redirect_to root_path
+    else
+      render component: 'ArtistUpdate', props: {artist: @artist, billboard: @billboard}
+    end
+  end
+
+  def destroy
+    @artist.destroy
+    redirect_to root_path
+  end
+
+  private
+    def set_billboard
+      @billboard = Billboard.find(params([:billboard_id])
+    end
+
+    def set_artist
+      @artist = Artist.find(params[:id])
+    end
+
+    def artist_params
+      params.require(:artist).permit(:first_name, :last_name)
+    end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,58 @@
 class SongsController < ApplicationController
+  before_action :set_artist
+  before_action :set_song, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @songs = @artist.songs
+    render component: 'Songs', props: {songs: @songs, artist: @artist}
   end
 
   def show
+    render component: 'Song', props: {song: @song, artist: @artist}
   end
 
   def new
+    @song = @artist.song.new
+    render component: 'SongNew', props: {song: @song, artist: @artist}
+  end
+
+  def create
+    @song = @artist.songs.new(song_params)
+    if @song.save
+      redirect_to root_path
+    else
+      render component: 'SongNew', props: {song: @song, artist: @artist}
+    end
   end
 
   def edit
+    render component: 'SongEdit', props: {song: @song, artist: @artist}
   end
+
+  def update
+    if @song.update(song_params)
+      redirect_to root_path
+    else
+      render component: 'SongUpdate', props: {song: @song, artist: @artist}
+    end
+  end
+
+  def destroy
+    @song.destroy
+    redirect_to root_path
+  end
+
+  private
+    def set_song
+      @artist = Artist.find(params([:artist_id])
+    end
+
+    def set_song
+      @song = song.find(params[:id])
+    end
+
+    def song_params
+      params.require(:song).permit(:title, :rank, :album)
+    end
 end
+
