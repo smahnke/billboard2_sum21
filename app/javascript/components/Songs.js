f8a1c7bf26571a6bfdb4823d31e3b91026ecdc0a
Steph Mahnke <mahnkes1@msu.edu>
2021-06-02 18:48:39 -0600

    added song js files

---
+++ app/javascript/components/Songs.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Songs = ({ artist, songs }) => {
+  const { first_name, last_name, id } = artist
+  const url = `/artists/${id}/songs`
+  return (
+    <>
+      <h1>Songs from {first_name}{last_name}</h1>
+      <a href={`/artists/${id}`}>Back to {first_name}{last_name}</a>
+      <br />
+      <a href= {url + '/new'}>Add New Song</a>
+      {
+        songs.map( (song) => (
+          <div>
+            <h3>{song.title} from the album "{song.album} was ranked {song.id}</h3>
+            <a href={url + '/' + song.id}>Show Song</a>
+            {' '}
+            <a href={url + '/' + song.id + '/edit'}>Edit Song</a>
+            {' '}
+            <a href={url + '/' + song.id} data-method="delete">Delete Song</a>
+          </div>
+        ))
+      }
+    </>
+  )
+}
+
+export default Songs;
